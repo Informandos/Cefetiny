59e6686936a9d9e68f38cb148840336810ad9269
analiju <jujucarvalho.souza@yahoo.com.br>
2018-04-10 14:30:54 -0300

    Add files via upload
    Inicio entrada de dados.

---
+++ Cefetiny/src/io/EntradaDados.java
@@ -0,0 +1,29 @@
+
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class EntradaDados {
+    
+    public static void leitor(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+
+            } else
+                break;
+            linha = buffRead.readLine();
+        }
+        buffRead.close();
+    }
+}
