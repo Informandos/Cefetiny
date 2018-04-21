58206325b6b528764ee3c2ed865c63b2cce65e62
ChoNBy <carlos.henrique1475@gmail.com>
2018-04-21 08:58:35 -0300

    Classes de memória movidas para o projeto Cefetiny

--- Cefetiny/src/io/EntradaDados.java
+++ Cefetiny/src/io/EntradaDados.java
@@ -1,16 +1,12 @@
 
 package io;
 
-import java.io.BufferedReader;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 
 
