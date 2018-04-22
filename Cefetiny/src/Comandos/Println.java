0061ef92a55066a4b854d7da0df7383de79778b3
Lucca Bessa <LuccaBessa@users.noreply.github.com>
2018-04-22 15:27:05 -0300

    Tirando uns erros muito loucos qu eo git deu durante o upload dos arquivos

--- Cefetiny/src/Comandos/Println.java
+++ Cefetiny/src/Comandos/Println.java
@@ -1,15 +1,14 @@
 package Comandos;
 
 public class Println{
-<<<<<<< HEAD
+
 
     public Println() {
         System.out.println("");
-=======
+    }    
     
     public void executaPrintln() {
         System.out.println("\n");
->>>>>>> d720be701257b90dc2937b876b3bb883eb856cfd
     }
     
 }
